a9230cb7189c4aa75135c8b9191c3a7774181442
Yumi Izumi <me@yumiizumi.com>
2023-05-06 23:31:36 -0400

    Add period for works

--- src/data/works.ts
+++ src/data/works.ts
@@ -2,21 +2,25 @@ const works: Work[] = [
   {
     title: "Subscription Manager",
     link: "https://github.com/bluelockorg/subscription-manager",
+    period: "2022",
     skills: ["ionic", "react", "ts"],
   },
   {
     title: "World of Story",
     role: "Full Stack Developer",
+    period: "2022 - 2023",
     link: "https://worldofstory.worldroad.org/",
     skills: ["astro", "react", "ts", "php", "mysql"],
   },
   {
     title: "GridSnap",
+    period: "since 2023",
     link: "https://gridsnap.yumi.to/",
     skills: ["react", "ts"],
   },
   {
     title: "Why Privacy Matters - Ted Talk",
+    period: "2020",
     role: "Speaker",
     link: "https://www.youtube.com/watch?v=cpK-XshFAms",
   },
