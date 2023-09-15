f375187890c8aeb65346b98b1829c5deffb54511
Yumi Izumi <me@yumiizumi.com>
2023-09-15 11:11:07 -0400

    Remove unnecessary code
    remove types for skill and work

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,3 @@
-type SkillValue = Readonly<
-  "react" | "ts" | "ionic" | "astro" | "php" | "mysql"
->;
-
-type Skill = Readonly<{
-  value: SkillValue;
-  label: string;
-  svg: string;
-}>;
-
-type Work = Readonly<{
-  title: string;
-  role?: string;
-  period?: string;
-  link?: string;
-  skills?: SkillValue[];
-  description?: string;
-}>;
-
 type Social = Readonly<{
   for: string;
   link: string;
