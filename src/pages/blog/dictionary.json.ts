3c1c94a3f44e46e71d27ba5d275be7f3b3d41ab7
Yumi Izumi <me@yumiizumi.com>
2023-10-22 00:53:42 -0400

    Refactor blog/dictionary.json

--- src/pages/blog/dictionary.json.ts
+++ src/pages/blog/dictionary.json.ts
@@ -5,7 +5,7 @@ export async function get(context: APIContext) {
   const posts = await getCollection("blog");
   const dictionary: { [key: string]: string } = {};
   posts.forEach((post) => {
-    dictionary[post.slug] = post.data.blogShort ?? post.slug;
+    dictionary[post.data.blogShort ?? post.slug] = post.slug;
   });
 
   return new Response(JSON.stringify(dictionary), {
