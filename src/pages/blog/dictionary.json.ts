899065f6d3e59f4f4d42457d0d494ecd7c8720c4
Yumi Izumi <me@yumiizumi.com>
2024-03-15 21:24:16 -0400

    fix: use upper case endpoint names
    lower case endpoint names are deprecated

--- src/pages/blog/dictionary.json.ts
+++ src/pages/blog/dictionary.json.ts
@@ -1,16 +1,16 @@
 import { getCollection } from "astro:content";
 import type { APIContext } from "astro";
 
-export async function get(context: APIContext) {
-  const posts = await getCollection("blog");
-  const dictionary: { [key: string]: string } = {};
-  posts.forEach((post) => {
-    dictionary[post.data.blogShort ?? post.slug] = post.slug;
-  });
+export async function GET(context: APIContext) {
+	const posts = await getCollection("blog");
+	const dictionary: { [key: string]: string } = {};
+	posts.forEach((post) => {
+		dictionary[post.data.blogShort ?? post.slug] = post.slug;
+	});
 
-  return new Response(JSON.stringify(dictionary), {
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+	return new Response(JSON.stringify(dictionary), {
+		headers: {
+			"content-type": "application/json",
+		},
+	});
 }
--- src/pages/og/[slug].png.ts
+++ src/pages/og/[slug].png.ts
@@ -3,15 +3,16 @@ import { getCollection, getEntryBySlug } from "astro:content";
 import { getOgImage } from "@/components/OgImage";
 
 export async function getStaticPaths() {
-  const posts = await getCollection("blog");
-  return posts.map((post) => ({ params: { slug: post.slug } }));
+	const posts = await getCollection("blog");
+	return posts.map((post) => ({ params: { slug: post.slug } }));
 }
 
-export async function get({ params }: APIContext) {
-  const post = await getEntryBySlug("blog", params.slug!);
-  const body = await getOgImage(post?.data.title ?? "Untitled");
-  return {
-    body,
-    encoding: "binary",
-  };
+export async function GET({ params }: APIContext) {
+	const post = await getEntryBySlug("blog", params.slug!);
+	const body = await getOgImage(post?.data.title ?? "Untitled");
+	return new Response(body, {
+		headers: {
+			"content-type": "image/png",
+		},
+	});
 }
